a02fcb3b68ce764d4dd67ce1945337cd093c852c
Victor Alves <victoralvssalcantara@gmail.com>
2022-11-07 15:56:10 -0300

    Primeiros arquivos

---
+++ empresas/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Tecnologias(models.Model):
+    tecnologia = models.CharField(max_length=30)
+
+    def __str__(self):
+            return self.tecnologia
+
+class Empresa(models.Model):
+    choices_nicho_mercado = (
+        ('M', 'Marketing'),
+        ('N', 'Nutrição'),
+        ('D', 'Design'),
+        ('T', 'Tecnologia')
+    )
+    logo = models.ImageField(upload_to="logo_empresa", null=True)
+    nome = models.CharField(max_length=30)
+    email = models.EmailField()
+    cidade = models.CharField(max_length=30)
+    endereco = models.CharField(max_length=30)
+    caracteristicas_empresa = models.TextField()
+    nicho_mercado = models.CharField(max_length=4, choices=choices_nicho_mercado)
+    tecnologias = models.ManyToManyField(Tecnologias)
+
+    def __str__(self):
+        return self.nome
+
+
+class Vagas(models.Model):
+    choices_experiencia = (
+        ('J', 'Júnior'),
+        ('P', 'Pleno'),
+        ('S', 'Sênior')
+    )
+
+    choices_status = (
+        ('I', 'Interesse'),
+        ('C', 'Currículo enviado'),
+        ('E', 'Entrevista'),
+        ('D', 'Desafio técnico'),
+        ('F', 'Finalizado')
+    )
+
+    empresa = models.ForeignKey(Empresa, null=True, on_delete=models.SET_NULL)
+    titulo = models.CharField(max_length=30)
+    nivel_experiencia = models.CharField(max_length=2, choices=choices_experiencia)
+    data_final =  models.DateField()
+    status = models.CharField(max_length=30, choices=choices_status)
+    tecnologias_dominadas = models.ManyToManyField(Tecnologias)
+    tecnologias_etudar = models.ManyToManyField(Tecnologias, related_name="estudar")
+
+    def __str__(self):
+         return self.titulo
---
+++ empresas/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Tecnologias, Empresa 
+from django.contrib import messages
+from django.contrib.messages import constants, DEFAULT_TAGS
+# Create your views here.
+
+
+def nova_empresa(request):
+    if request.method == "GET":
+        techs = Tecnologias.objects.all
+        nichos = Empresa.choices_nicho_mercado
+        return render(request, 'nova_empresa.html', {'techs': techs, 'nichos': nichos})
+
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        cidade = request.POST.get('cidade')
+        endereco = request.POST.get('endereco')
+        nicho = request.POST.get('nicho')
+        caracteristicas = request.POST.get('caracteristicas')
+        tecnologias = request.POST.getlist('tecnologias') #GETLIST É USADO APRA CAMPOS COM MAIS DE UM VALOR
+        logo = request.FILES.get('logo')
+
+        
+        # VALIDAÇÕES
+
+        if (len(nome.strip()) == 0 or len(email.strip()) == 0 or len(cidade.strip()) == 0 or len(endereco.strip()) == 0 or len(nicho.strip()) == 0 or len(caracteristicas.strip()) == 0 or (not logo)): 
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+            return redirect('/home/nova_empresa')
+
+        if logo.size > 100_000_000:
+            messages.add_message(request, constants.ERROR, 'A logo da empresa deve ter menos de 10MB')
+            return redirect('/home/nova_empresa')
+
+        if nicho not in [i[0] for i in Empresa.choices_nicho_mercado]:
+            messages.add_message(request, constants.ERROR, 'Nicho de mercado inválido')
+            return redirect('/home/nova_empresa')
+
+        empresa = Empresa(logo=logo,
+                            nome=nome,
+                            email=email,
+                            cidade=cidade,
+                            endereco=endereco,
+                            nicho_mercado=nicho,
+                            caracteristicas_empresa =caracteristicas
+                        )
+
+        empresa.save()
+        empresa.tecnologias.add(*tecnologias)
+        empresa.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Empresa cadastrada com sucesso!')
+        
+        return redirect('/home/nova_empresa')
+
+       
+def empresa(request):
+    empresas = Empresa.objects.all()
+    tecnologias = Tecnologias.objects.all()
+    return render(request, 'empresa.html', {'empresas': empresas, 'tecnologias': tecnologias})
+
+def excluir_empresa(request, id):
+    empresa = Empresa.objects.get(id=id)
+    print(id)
+    empresa.delete()
+    messages.add_message(request, constants.SUCCESS, 'Empresa deletada com sucesso!')
+    
+    return redirect('/home/empresas')
